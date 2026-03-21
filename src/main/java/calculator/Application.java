f96061178008b98461530af7be4889a4f5754bf6
dong279 <dongho0130@daum.net>
2026-03-21 19:57:29 +0900

    '//'와'|n'사이의 문자를 커스텀 구분자로 추출하는 로직 구현 완료. + Pattern.quote()로 특수문자도 구분자로 사용 가능하게 처리함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
     }
 
@@ -15,6 +15,16 @@ public class Application {
 
         String delimiter = "[,:]";
         String numberPart = input;
+
+        if(input.startsWith("//")) {
+            int idx = input.indexOf("\\n");
+            if (idx < 0) {
+                throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
+            }
+            String custom = input.substring(2,idx);
+            delimiter = "[,:]|" + java.util.regex.Pattern.quote(custom);
+            numberPart = input.substring(idx+2);
+        }
 
         int sum = 0;
         for (String token : numberPart.split(delimiter)) {
