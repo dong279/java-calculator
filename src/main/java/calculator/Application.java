6a24e8a169d49c8932ce8dceed36c037fb042420
dong279 <dongho0130@daum.net>
2026-03-21 19:58:44 +0900

    결과물 출력 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        int result = calculate(input);
+        System.out.println("결과: " + result);
     }
 
     private static int calculate(String input) {
