50ebce160f2f2f4fdeafdafc863f879e20d0187f
dong279 <dongho0130@daum.net>
2026-03-24 22:43:11 +0900

    feat: StringCalculator 클래스를 생성하여 숫자 합산 계산 로직 분리

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -1,4 +1,29 @@
 package calculator.model;
 
 public class StringCalculator {
+
+  public static int calculate(String input) {
+    if (input == null || input.isEmpty()) {
+      return 0;
+    }
+    String[] tokens = StringParser.parse(input);
+    return sum(tokens);
+  }
+  private static int sum(String[] tokens) {
+    int total = 0;
+    for (String token : tokens) {
+      int number = parseNumber(token);
+      Validator.validateNotNegative(number);
+      total += number;
+    }
+    return total;
+  }
+
+  private static int parseNumber(String token) {
+    try{
+      return Integer.parseInt(token.trim());
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다." + token);
+    }
+  }
 }
