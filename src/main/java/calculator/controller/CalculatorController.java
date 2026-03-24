dc695900531e1f9aef05fe87655b60b1ef37251a
dong279 <dongho0130@daum.net>
2026-03-24 22:27:59 +0900

    feat: Validator 클래스 생성으로 커스텀 구분자 및 음수 검증 분리

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,13 @@
 package calculator.controller;
 
+import calculator.model.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+    public void run(){
+        String input = InputView.read();
+        int result = StringCalculator.calculate(input);
+        OutputView.printResult(result);
+    }
 }
--- src/main/java/calculator/model/Validator.java
+++ src/main/java/calculator/model/Validator.java
@@ -1,4 +1,14 @@
 package calculator.model;
 
 public class Validator {
+    public static void validateCustomDelimiterFormat(int newlineIndex){
+        if(newlineIndex < 0){
+            throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
+        }
+    }
+    public static void validateNotNegative(int number){
+        if(number < 0){
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        }
+    }
 }
