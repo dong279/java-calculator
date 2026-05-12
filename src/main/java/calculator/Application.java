06f62a27a55484a3eb794cc5e668a2b2e3cc654b
dong279 <dongho0130@daum.net>
2026-05-12 19:21:55 +0900

    - StringCalulator를 static 직접 호출에서 생성자 주입(DI)로 변경 - Controller가 구체 구현이 아닌 주입된 인스턴스에 의존하도록 수정함 DI 적용으로 단위 테스트 용이성 및 유연성 확보

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,12 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 import calculator.controller.CalculatorController;
+import calculator.model.StringCalculator;
 
 public class Application {
     public static void main(String[] args) {
         try {
-            CalculatorController controller = new CalculatorController();
+            CalculatorController controller = new CalculatorController(new StringCalculator());
             controller.run();
         } finally {
             Console.close();
--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -5,6 +5,13 @@ import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculatorController {
+
+    private final StringCalculator calculator;
+
+    public CalculatorController(StringCalculator calculator) {
+        this.calculator = calculator;
+    }
+
     public void run(){
         String input = InputView.read();
         int result = StringCalculator.calculate(input);
